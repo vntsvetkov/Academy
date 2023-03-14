fd734a872f9bcc0f6c0ccedcd941bb94b042149b
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-03-14 21:00:58 +0300

    реализовал класс ITCompany

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 """
 Object oriented programming
 """
+from typing import List
 # Задача 1. Описать класс программист.
 class Programmer:
 
@@ -20,10 +21,42 @@ class Programmer:
         self.language = language
         self.company = company
 
+    def __str__(self):
+        return f"Имя: {self.name} \n" \
+               f"Возраст: {self.age} \n" \
+               f"Пол: {self.gender} \n" \
+               f"Язык программирования: {self.language} \n" \
+               f"Компания: {self.company}"
+
+# Задача 1.1 Создать класс IT-Компания
+class ITCompany:
+    name: str
+    address: str # лучше словарь
+    programmers: List[Programmer]
+
+    def __init__(self, name: str, address: str, programmers: list = None):
+        self.name = name
+        self.address = address
+        if programmers is None:
+            self.programmers = []
+        else:
+            self.programmers = programmers.copy()
+
+    def __str__(self):
+        return f"Название компании: {self.name} \n" \
+               f"Адрес компании: {self.address} \n"
+
 
 def execute_application():
-    programmer = Programmer("Иван", 32, "male", "Python", "Yandex")
-    print(programmer.__dict__)
+    programmer1 = Programmer("Иван", 32, "male", "Python", "Yandex")
+    programmer2 = Programmer("Петр", 36, "male", "C++", "Yandex")
+
+    company = ITCompany("Yandex", "119021, Москва, ул. Льва Толстого, 16")
+    company.programmers.append(programmer1)
+    company.programmers.append(programmer2)
+    print(company)
+    for programmer in company.programmers:
+        print(programmer)
 
 
 if __name__ == "__main__":
