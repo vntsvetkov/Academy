7d0a78292612e930c7bb09d6cacef9dc7bd014f4
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-03-14 20:24:23 +0300

    Создал конструктор класса

--- main.py
+++ main.py
@@ -4,22 +4,28 @@ Object oriented programming
 """
 # Задача 1. Описать класс программист.
 class Programmer:
-    # Поля класса
+
     name: str
     age: int
     gender: str
     language: str
     company: str
 
+    def __init__(self, name: str, age: int, gender: str,
+                language: str, company: str):
+        """Конструктор класса Programmer"""
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.language = language
+        self.company = company
+
 
 def execute_application():
     programmer = Programmer()
-    programmer.name = "Николай"
-    programmer.age = 28
-    programmer.gender = "male"
-    programmer.language = "Python"
-    programmer.company = "Tensor"
     print(programmer.__dict__)
+    programmer1 = Programmer()
+    print(programmer1.__dict__)
 
 
 if __name__ == "__main__":
