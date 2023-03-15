e799c663ce88eb208989a4ca1f8762b00368f995
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-03-16 00:28:32 +0300

    Внёс корректировки в код по заданию 1

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-""" Модуль 9. День 1"""
-"""
-Object oriented programming
-"""
+""" Модуль 9. День 1. Object oriented programming"""
+
 from typing import List
-# Задача 1. Описать класс программист.
+# Задача 1. Описать класс программист. Описать класс IT-компания.
 class Programmer:
 
     name: str
@@ -14,7 +12,6 @@ class Programmer:
 
     def __init__(self, name: str, age: int, gender: str,
                 language: str, company: str = None):
-        """Конструктор класса Programmer"""
         self.name = name
         self.age = age
         self.gender = gender
@@ -28,13 +25,13 @@ class Programmer:
                f"Язык программирования: {self.language} \n" \
                f"Компания: {self.company}"
 
-# Задача 1.1 Создать класс IT-Компания
+
 class ITCompany:
     name: str
-    address: str # лучше словарь
+    address: str
     programmers: List[Programmer]
 
-    def __init__(self, name: str, address: str, programmers: list = None):
+    def __init__(self, name: str, address: str, programmers: List[Programmer] = None):
         self.name = name
         self.address = address
         if programmers is None:
@@ -50,7 +47,6 @@ class ITCompany:
 def execute_application():
     programmer1 = Programmer("Иван", 32, "male", "Python", "Yandex")
     programmer2 = Programmer("Петр", 36, "male", "C++", "Yandex")
-
     company = ITCompany("Yandex", "119021, Москва, ул. Льва Толстого, 16")
     company.programmers.append(programmer1)
     company.programmers.append(programmer2)
