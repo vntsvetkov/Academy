37a9d2791f1a0652dbc5daa424c7fb5aa38c3919
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-03-16 19:00:53 +0300

    Добавил класс Programmer

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+class Programmer:
+
+    name: str
+    age: int
+    gender: str
+    language: str
+    company: str
+
+    def __init__(self, name: str, age: int, gender: str,
+                language: str, company: str = None):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.language = language
+        self.company = company
+
+    def __str__(self):
+        return f"Имя: {self.name} \n" \
+               f"Возраст: {self.age} \n" \
+               f"Пол: {self.gender} \n" \
+               f"Язык программирования: {self.language} \n" \
+               f"Компания: {self.company}"
+
 
 
 def execute_application():
