692e3b2eae1239b4c5a35f091e8473c085cf44c7
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-03-16 20:17:03 +0300

    Реализовали расчет зарплаты

--- main.py
+++ main.py
@@ -4,13 +4,15 @@
 class Programmer:
 
     def __init__(self, name: str, age: int, gender: str,
-                 language: str, company: str, salary: float):
+                 language: str, company: str,
+                 salary: float = 0, premium: float = 0):
         self.name = name
         self.age = age
         self.gender = gender
         self.language = language
         self.company = company
         self.salary = salary
+        self.premium = premium
 
     def __str__(self):
         return f"Имя: {self.name} \n" \
@@ -18,17 +20,17 @@ class Programmer:
                f"Пол: {self.gender} \n" \
                f"Язык программирования: {self.language} \n" \
                f"Компания: {self.company} \n" \
-               f"Заработная плата: {self.salary}"
+               f"Заработная плата: {self.salary + self.premium}"
 
-    def get_premium(self, percent: float):
+    def calc_premium(self, percent: float = 0):
         return self.salary * percent / 100
 
 
 def execute_application():
     programmer = Programmer("Иван", 32, "male", "Python", "Яндекс", 100000)
-    premium = programmer.get_premium(10)
+    premium = programmer.calc_premium(5)
+    programmer.premium = premium
     print(programmer)
-    print(f"Премия: {premium}")
 
 
 if __name__ == "__main__":
