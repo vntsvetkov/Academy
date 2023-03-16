4438fc39f343a2b704354a85e3975da0959667c2
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-03-16 20:42:19 +0300

    Описал геттер и сеттер для поля премия

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 """ Модуль 9. День 2. Object oriented programming"""
-
+"""
+Модификаторы доступа:
+    public - общедоступный модификатор
+    private - обращение доступно только внутри класса
+    protected - обращение доступно только внутри класса и класса наследника
+"""
 
 class Programmer:
 
     def __init__(self, name: str, age: int, gender: str,
                  language: str, company: str,
                  salary: float = 0, premium: float = 0):
-        self.name = name
-        self.age = age
-        self.gender = gender
-        self.language = language
-        self.company = company
-        self.salary = salary
-        self.premium = premium
+        self.__name = name
+        self.__age = age
+        self.__gender = gender
+        self.__language = language
+        self.__company = company
+        self.__salary = salary
+        self.__premium = premium
 
     def __str__(self):
-        return f"Имя: {self.name} \n" \
-               f"Возраст: {self.age} \n" \
-               f"Пол: {self.gender} \n" \
-               f"Язык программирования: {self.language} \n" \
-               f"Компания: {self.company} \n" \
-               f"Заработная плата: {self.salary + self.premium}"
+        return f"Имя: {self.__name} \n" \
+               f"Возраст: {self.__age} \n" \
+               f"Пол: {self.__gender} \n" \
+               f"Язык программирования: {self.__language} \n" \
+               f"Компания: {self.__company} \n" \
+               f"Заработная плата: {self.__salary + self.__premium}"
 
-    def calc_premium(self, percent: float = 0):
-        return self.salary * percent / 100
+    def __calc_premium(self, percent: float = 0):
+        return self.__salary * percent / 100
+
+    def get_premium(self):
+        return self.__premium
+
+    def set_premium(self, percent: float):
+        self.__premium = Programmer.__calc_premium(self, percent)
 
 
 def execute_application():
     programmer = Programmer("Иван", 32, "male", "Python", "Яндекс", 100000)
-    premium = programmer.calc_premium(5)
-    programmer.premium = premium
+    programmer.set_premium(10)
+    print(programmer.get_premium())
     print(programmer)
-
 
 if __name__ == "__main__":
     execute_application()
