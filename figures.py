5ed01bd8309659ba640cf1f4810470625c3a9314
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-03-23 21:10:47 +0300

    Реализовали методы класса triangle

--- figures.py
+++ figures.py
@@ -1,9 +1,23 @@
 from math import sqrt
 class Triangle:
+    __color: str
+    __ANGLE_SUM: int = 180
     def __init__(self, a: float, b: float, c: float):
         self.__a = a
         self.__b = b
         self.__c = c
+
+    @property
+    def a(self):
+        return self.__a
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def c(self):
+        return self.__c
 
     def height(self, key: str):
         side = {"a": self.__a, "b": self.__b, "c": self.__c}
@@ -17,6 +31,16 @@ class Triangle:
         else:
             raise Exception("Треугольника с данными сторонами не существует")
 
+    @classmethod
+    def create_triangle_by_angles(cls, a: float, angleA: int, angleB: int):
+        b = None
+        c = None
+        return cls(a, b, c)
+
+    def __str__(self):
+        return f"Сторона a: {self.__a} \n" \
+               f"Сторона b: {self.__b} \n" \
+               f"Сторона с: {self.__c} \n"
 
 class Circle:
     pass
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from geometry_square import GeometrySquare
+from figures import Triangle
 
 
 def execute_application():
-    pass
+    a = float(input("Введите сторону треугольника A: "))
+    b = float(input("Введите сторону треугольника B: "))
+    c = float(input("Введите сторону треугольника C: "))
+    try:
+        triangle = Triangle.is_triangle(a, b, c)
+        triangle.h_a = triangle.height("a")
+        triangle.square = GeometrySquare.triangle_square_1(triangle.a, triangle.h_a)
+        print(triangle)
+        print(f"Площадь: {triangle.square} м.кв")
+    except Exception as e:
+        print(e)
+
+
 
 
 if __name__ == "__main__":
