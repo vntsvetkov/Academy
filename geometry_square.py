1ea9577445e381bfdb7da0ea7fd1f99f3b7cf521
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-03-23 19:31:13 +0300

    Реализовал статические методы для площади окружности

--- geometry_square.py
+++ geometry_square.py
@@ -1,9 +1,8 @@
-from math import sin
+from math import sin, pi
 """Создайте класс для подсчета площади геометрических
 фигур. Класс должен предоставлять функциональность
 для подсчета площади треугольника по разным формулам,
-площади прямоугольника, площади квадрата, площади
-ромба. Методы класса для подсчета площади должны
+площади круга. Методы класса для подсчета площади должны
 быть реализованы с помощью статических методов."""
 
 
@@ -20,3 +19,15 @@ class GeometrySquare:
     @staticmethod
     def triangle_square_3(a: float, angleA: int, angleB: int):
         return 0.5 * a * a * (sin(angleA) * sin(angleB)) / sin(angleA + angleB)
+
+    @staticmethod
+    def circle_square_1(d: float):
+        return 0.25 * pi * d ** 2
+
+    @staticmethod
+    def circle_square_2(l: float, d: float):
+        return 0.25 * l * d
+
+    @staticmethod
+    def circle_square_2(r: float):
+        return pi * r ** 2
