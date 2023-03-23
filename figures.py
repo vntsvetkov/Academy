036625a2a8c29edb180c584aa0658ddffa5edeed
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-03-23 20:38:50 +0300

    Реализовал класс triangle"

--- figures.py
+++ figures.py
@@ -1,5 +1,21 @@
+from math import sqrt
 class Triangle:
-    pass
+    def __init__(self, a: float, b: float, c: float):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+
+    def height(self, key: str):
+        side = {"a": self.__a, "b": self.__b, "c": self.__c}
+        p = (self.__a + self.__b + self.__c) / 2
+        return 2 * sqrt(p*(p-self.__a)*(p-self.__b)*(p-self.__c)) / side[key]
+
+    @staticmethod
+    def is_triangle(a: float, b: float, c: float):
+        if (a < b + c) and (b < a + c) and (c < a + b):
+            return Triangle(a, b, c)
+        else:
+            raise Exception("Треугольника с данными сторонами не существует")
 
 
 class Circle:
