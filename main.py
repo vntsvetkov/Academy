6166bcb777b0aa8e843890cbc588e92c763cf047
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-03-25 14:36:46 +0300

    Ообновил файл main.py

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from figures import Triangle
 
 
 def execute_application():
-    a = float(input("Введите сторону треугольника A: "))
-    b = float(input("Введите сторону треугольника B: "))
-    c = float(input("Введите сторону треугольника C: "))
-    try:
-        triangle = Triangle.is_triangle(a, b, c)
-        triangle.h_a = triangle.height("a")
-        triangle.square = GeometrySquare.triangle_square_1(triangle.a, triangle.h_a)
-        print(triangle)
-        print(f"Площадь: {triangle.square} м.кв")
-    except Exception as e:
-        print(e)
 
+    triangle = Triangle()
+    while True:
+        triangle.a = float(input("Введите сторону треугольника A (м): "))
+        triangle.b = float(input("Введите сторону треугольника B (м): "))
+        triangle.c = float(input("Введите сторону треугольника C (м): "))
+        if Triangle.is_triangle(triangle.a, triangle.b, triangle.c):
+            break
+        else:
+            print("Треугольника с такими сторонами не существует")
 
+    triangle.h_a = triangle.height("a")
+    triangle.square = GeometrySquare.triangle_square_by_side_and_height(triangle.a, triangle.h_a)
+    print(f"Площадь: {triangle.square} (м.кв)")
 
 
 if __name__ == "__main__":
