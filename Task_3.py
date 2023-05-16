89058664b070856958b1fc7599719a7bc505b4d4
nv.tsvetkov <nv.cvetkov@tensor.ru>
2023-05-16 11:48:58 +0300

    Добавил файл Task3.py

---
+++ Task_3.py
@@ -0,0 +1,31 @@
+class ExpressionConverter:
+    operation_priority = {
+        '(': 0,
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+    }
+
+    @staticmethod
+    def to_postfix(expression: str) -> list:
+        pass
+
+    @staticmethod
+    def to_infix(expression: list) -> str:
+        pass
+
+
+class Expression:
+
+    def __init__(self, expression: str):
+        self.__infix_expression = expression
+        self.__postfix_expression = ExpressionConverter.to_postfix(expression)
+
+    @property
+    def infix_expression(self):
+        return self.__infix_expression
+
+    @property
+    def postfix_expression(self):
+        return self.__postfix_expression
