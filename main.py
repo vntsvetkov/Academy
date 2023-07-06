9a677cb17ac4405d6e0bc012e6a464182000a014
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-07-06 21:05:12 +0300

    Добавил скрипты основных операций с БД

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ def create_table(cursor, sql_request: str):
     print(f"Таблица создана")
 
 
+def check_phone(cursor, phone: str, table: str) -> bool:
+    cursor.execute(f"""SELECT * FROM {table} WHERE phone = ?""", (phone, ))
+
+    return cursor.fetchall() == []
+
+
 def execute_application():
 
     # Задача 1. Создать таблицу
@@ -85,7 +91,6 @@ def execute_application():
     except Error as e:
         print(e)
     finally:
-
         connection.commit()  # зафиксировать изменения в БД перед закрытием подключения.
         # Выполняется, если в БД происходят изменения без использования with
         cursor.close()
@@ -169,12 +174,119 @@ def execute_application():
     except Error as e:
         print(e)
     else:
-        print(fields)
+        for field in fields:
+            print(field[1])
     finally:
         cursor.close()
         connection.close()
         print("Соединение завершено")
 
+    # Задача 5. Удаление строки данных из таблицы
+    connection = sqlite3.connect('phonebook.db')
+    cursor = connection.cursor()
+    table_name = "Contacts"
+    city = input("Введите название города: ")
+    try:
+        # Cделать выборку по введнному city
+        # sql_request = f""" SELECT * FROM {table_name}
+        #                    WHERE city = ?
+        #                     """
+        # cursor.execute(sql_request, (city, ))
+        # data = cursor.fetchall()
+        # Подключиться к резервной базе и добавить туда
+
+        sql_request = f""" DELETE FROM {table_name} 
+                           WHERE city = ?
+                       """
+        cursor.execute(sql_request, (city,))
+    except Error as e:
+        print(e)
+    else:
+        print("Запрос на удаление выполнен")
+    finally:
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    # Задача 6. Обновление данных. Поменять номер телефона пользователя
+    connection = sqlite3.connect('phonebook.db')
+    cursor = connection.cursor()
+    table_name = "Contacts"
+    last_phone = "456-678"
+    new_phone = input("Введите новый номер: ")
+
+    if check_phone(cursor, new_phone, table_name):
+
+        try:
+            sql_request = f""" UPDATE {table_name}
+                               SET phone = ?
+                               WHERE phone = ? 
+                           """
+            cursor.execute(sql_request, (new_phone, last_phone))
+        except Error as e:
+            print(e)
+        else:
+            print("Номер телефона изменен")
+        finally:
+            connection.commit()
+            cursor.close()
+            connection.close()
+
+    else:
+        print("Такой номер уже сществует, введите другой")
+        cursor.close()
+        connection.close()
+
+    # Добавление столбца
+    connection = sqlite3.connect('phonebook.db')
+    cursor = connection.cursor()
+    table_name = "Contacts"
+    column_name = "street"
+    try:
+        sql_request = f"""ALTER TABLE {table_name}
+                          ADD COLUMN {column_name} TEXT
+                       """
+        cursor.execute(sql_request)
+    except Error as e:
+        print(e)
+    else:
+        print(f"Столбец {column_name} добавлен")
+    finally:
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    # Выполнение скрипта на добавление таблицы
+    sqlite_connection = sqlite3.connect('phonebook.db')
+    cursor = sqlite_connection.cursor()
+    with open('sqlite_create_tables.sql', 'r') as sqlite_file:
+        sql_script = sqlite_file.read()
+
+    try:
+        cursor.executescript(sql_script)
+        print("Скрипт SQLite успешно выполнен")
+    except Error as error:
+        print("Ошибка при подключении к sqlite", error)
+    finally:
+        cursor.close()
+        sqlite_connection.close()
+        print("Соединение с SQLite закрыто")
+
+    # Бэкап всей базы
+
+    sqlite_con = sqlite3.connect('phonebook.db')
+    backup_con = sqlite3.connect('phonebook_backup.db')
+    with backup_con:
+        try:
+            sqlite_con.backup(backup_con)
+            print("Резервное копирование выполнено успешно")
+        except Error as error:
+            print("Ошибка при резервном копировании: ", error)
+        finally:
+            backup_con.close()
+            sqlite_con.close()
+
 
 if __name__ == "__main__":
     execute_application()
+
