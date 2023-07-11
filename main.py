e13498bfb78a3cbfba9212adadce266579132744
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-07-11 21:16:29 +0300

    Реализовали класс db_phonebook

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+from phonebook import DBPhonebook, Entry, Phonebook
 
 
 def execute_application():
-    pass
+
+    db_phonebook = DBPhonebook()
+    db_phonebook.connect('phonebook.db')
+    db_phonebook.create_cursor()
+
+    new_entry = Entry("Tom", "Jones", "456-988", "New York", "5 Avenu")
+    db_phonebook.add_entry(new_entry.get_tuple_entry())
+
+    data = db_phonebook.get_data()
+    current_phonebook = Phonebook.create_from_execute(data)
+
+    db_phonebook.close_cursor()
 
 
 if __name__ == "__main__":
--- phonebook.py
+++ phonebook.py
@@ -2,11 +2,13 @@ import sqlite3
 
 
 class Entry:
-    def init(self, name, surname, phone, city):
+    def __init__(self, name, surname, phone, city, street):
+        # вместо полей сделать словарь
         self.name = name
         self.surname = surname
         self.phone = phone
         self.city = city
+        self.street = street
 
     @classmethod
     def from_json(cls, path: str):
@@ -15,6 +17,20 @@ class Entry:
     @classmethod
     def from_txt(cls, path: str):
         pass
+
+    def get_tuple_entry(self) -> tuple:
+        # Преобразование словаря к кортежу
+        return self.name, self.surname, self.phone, self.city, self.street
+
+
+class DBSingleton:
+    __connection: sqlite3.Connection = None
+
+    @classmethod
+    def connect(cls, path):
+        if not cls.__connection:
+            cls.__connection = sqlite3.connect(path)
+        return cls.__connection
 
 
 class DBPhonebook:
@@ -28,8 +44,30 @@ class DBPhonebook:
     def create_cursor(self):
         self.__cursor = self.__connection.cursor()
 
-    def add_entry(self):
+    def create_table(self, path: str):
+        # можно проверить существование файла
+        with open(path, 'r') as sqlite_file:
+            sql_script = sqlite_file.read()
+
+        if self.__cursor is not None:
+            self.__cursor.executescript(sql_script)
+
+    def add_entry(self, data: tuple):
+        if self.__cursor is not None:
+            self.__cursor.execute(f""" INSERT INTO Contacts (name, surname, phone, city, street)
+                                        VALUES(?, ?, ?, ?, ?)""", data)
+            self.__connection.commit()
+            print("Запись добавлена")
+
+    def add_list_entry(self, data: list[tuple]):
         pass
+
+    def get_data(self) -> list[tuple]:
+        if self.__cursor is not None:
+            sql_request = """ SELECT * FROM Contacts """
+            self.__cursor.execute(sql_request)
+            # return self.__cursor.fetchone()
+            return self.__cursor.fetchall()
 
     def close_connection(self):
         self.__connection.close()
@@ -45,6 +83,11 @@ class Phonebook:
 
     __data: list[Entry] = []
 
+    @classmethod
+    def create_from_execute(cls, data: list[tuple]):
+        # конвертация из list[tuple] в объект list[Entry]
+        pass
+
     def add_entry(self, entry: Entry):
         pass
 
@@ -56,3 +99,4 @@ class Phonebook:
 
     def get_data(self):
         return self.__data
+
