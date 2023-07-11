33feaf4835d1120a76dfdad2f65f5d627d49afde
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-07-11 19:48:44 +0300

    Добавил описания классов

--- phonebook.py
+++ phonebook.py
@@ -1,7 +1,58 @@
+import sqlite3
+
+
 class Entry:
-    pass
+    def init(self, name, surname, phone, city):
+        self.name = name
+        self.surname = surname
+        self.phone = phone
+        self.city = city
+
+    @classmethod
+    def from_json(cls, path: str):
+        pass
+
+    @classmethod
+    def from_txt(cls, path: str):
+        pass
+
+
+class DBPhonebook:
+
+    __connection: sqlite3.Connection = None
+    __cursor: sqlite3.Cursor = None
+
+    def connect(self, path: str):
+        self.__connection = sqlite3.connect(path)
+
+    def create_cursor(self):
+        self.__cursor = self.__connection.cursor()
+
+    def add_entry(self):
+        pass
+
+    def close_connection(self):
+        self.__connection.close()
+
+    def close_cursor(self):
+        self.__cursor.close()
+
+    def __del__(self):
+        self.__connection.close()
 
 
 class Phonebook:
-    pass
 
+    __data: list[Entry] = []
+
+    def add_entry(self, entry: Entry):
+        pass
+
+    def delete_entry(self):
+        pass
+
+    def update_entry(self):
+        pass
+
+    def get_data(self):
+        return self.__data
